84382926de061d6fdb026b057cd6c25cc23ba46a
G.Villeroy <villeroy.gab@gmail.com>
2025-09-18 11:19:32 +0200

    caca du coup

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Exo1();
+    }
+
+    public void Exo1()
+    {
+        Console.WriteLine("caca!");
+    }
+}
+
