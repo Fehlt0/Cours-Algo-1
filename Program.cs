a34559a7cc942b08d0f9169ab947bb842e198e01
G.Villeroy <villeroy.gab@gmail.com>
2025-09-18 13:02:43 +0200

    input + check

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 ﻿public class Program
 {
+    private const int maxValue = 874;
+    private const int minValue = 6;
+    private static string inputStr;
+    
     public static void Main(string[] args)
     {
-        Exo1();
+        Program program = new Program();
+        program.Exo1();
     }
-
+    
     public void Exo1()
     {
-        Console.WriteLine("caca!");
+        Console.Write("enter value : \n ");
+        inputStr = Console.ReadLine();
+        Console.Clear();
+        
+        if (int.TryParse(inputStr, out int userInt)) return ;
+        if (userInt < minValue || userInt > maxValue) return;
+        
+        Console.Write("value is valid \n");
     }
 }
-
